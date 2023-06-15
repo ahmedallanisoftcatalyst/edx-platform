8b471d4eeb86f39b1da325d97239d7b2f87d733e
Bernard Szabo <bszabo@edx.org>
2023-06-15 13:08:12 -0400

    feat: TNL-10639 .content.json() fix

--- cms/djangoapps/contentstore/management/commands/delete_library.py
+++ cms/djangoapps/contentstore/management/commands/delete_library.py
@@ -60,6 +60,6 @@ class Command(BaseCommand):
             exit(0)
 
         response_format = 'json'
-        json_info = library_blocks_view(library, 'delete_library management command', response_format).response.json()
+        json_info = library_blocks_view(library, 'delete_library management command', response_format).content.json()
         print(json.dumps(json_info, indent = 2))
         print("****** Done looking for block info ******")
